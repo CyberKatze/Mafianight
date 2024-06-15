5448c4816df14859f71550ab3f9b31768384b05b
Zahra Khodabakhshian <zahrakhodabakhshian54@gmail.com>
2024-06-15 21:10:24 +0200

    add Signup form

--- client/src/components/Loginform.tsx
+++ client/src/components/Loginform.tsx
@@ -3,24 +3,35 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import hat from "../assets/images/hat.png"
-
+import { useState } from 'react';
 
 
 const loginSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
   password: z.string().min(6, { message: 'Password must be at least 6 characters long' }),
+  confirmPassword: z.string().min(6, { message: 'Password must be at least 6 characters long' })
+});
+const signupSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters long' }),
+  confirmPassword: z.string().min(6, { message: 'Password must be at least 6 characters long' }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ["confirmPassword"],
 });
 
 
-type FormData = z.infer<typeof loginSchema>;
+type LoginFormData = z.infer<typeof loginSchema>;
+type SignupFormData = z.infer<typeof signupSchema>;
 
+type FormData = LoginFormData | SignupFormData;
 
 
 const Loginform = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const formOptions = isSignUp ? { resolver: zodResolver(signupSchema) } : { resolver: zodResolver(loginSchema) };
 
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-    resolver: zodResolver(loginSchema),
-  });
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>(formOptions);
 
 
   const onSubmit = (data: FormData) => {
@@ -28,45 +39,59 @@ const Loginform = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="#" className="flex items-center mb-6 text-2xl font-semibold  text-white">
           <img className="w-8 h-8 mr-2 bg-black rounded-full" src={hat} alt="logo" />
           Mafia
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full  rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+            <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
+              {isSignUp ? 'Create an account' : 'Sign in to your account'}
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium  text-white">Your email</label>
                 <input
                   type="email"
                   id="email"
                   {...register('email')}
-                  className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.email ? 'border-red-500' : ''}`}
+                  className={` border sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 ${errors.email ? 'border-red-500' : ''}`}
                   placeholder="name@.com"
                   required
                 />
-                {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
+                {errors.email && <p className="text-red text-sm mt-1">{errors.email.message}</p>}
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium  text-white">Password</label>
                 <input
                   type="password"
                   id="password"
                   {...register('password')}
-                  className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.password ? 'border-red-500' : ''}`}
+                  className={` border  sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 ${errors.password ? 'border-red-500' : ''}`}
                   placeholder="••••••••"
                   required
                 />
-                {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
+                {errors.password && <p className="text-red text-sm mt-1">{errors.password.message}</p>}
               </div>
-              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+              {isSignUp && ( 
+                <div>
+                  <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-white">Confirm Password</label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    {...register('confirmPassword')}
+                    className={` border  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 ${errors.confirmPassword ? 'border-red-500' : ''}`}
+                    placeholder="••••••••"
+                    required
+                  />
+                  {errors.confirmPassword && <p className="text-red text-sm mt-1">{errors.confirmPassword.message}</p>}
+                </div>
+              )}
+              <button  type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{isSignUp ?  'Sign up' : 'Sign in'}</button>
+              <p className="text-sm font-light text-gray-400">
+              {isSignUp ? 'Already have an account?' : "Don’t have an account yet?"} <a onClick={() => setIsSignUp(!isSignUp)} href="#" className="font-medium  hover:underline text-primary-500">{isSignUp ? 'Sign in' : 'Sign up'}</a>
               </p>
             </form>
           </div>
@@ -74,6 +99,5 @@ const Loginform = () => {
       </div>
     </section>
   );
-}
-
+} 
 export default Loginform
