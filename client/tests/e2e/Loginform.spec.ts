84588eec0f3c91b267246ff45567461d2a5549f5
Zahra Khodabakhshian <zahrakhodabakhshian54@gmail.com>
2024-07-21 21:00:48 +0200

    login form test

--- client/tests/e2e/Loginform.spec.ts
+++ client/tests/e2e/Loginform.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.pause();
@@ -6,12 +6,21 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Log in' }).click();
   await page.getByRole('link', { name: 'Sign up' }).click();
   await page.getByPlaceholder('name@.com').click();
-  await page.getByPlaceholder('name@.com').fill('zara@gmail.com');
+  await page.getByPlaceholder('name@.com').fill('userr123@gmail.com');
   await page.getByPlaceholder('Your username').click();
-  await page.getByPlaceholder('Your username').fill('zara');
+  await page.getByPlaceholder('Your username').fill('userr');
   await page.getByLabel('Password', { exact: true }).click();
-  await page.getByLabel('Password', { exact: true }).fill('qwerty');
+  await page.getByLabel('Password', { exact: true }).fill('1234567');
   await page.getByLabel('Confirm Password').click();
-  await page.getByLabel('Confirm Password').fill('qwerty');
+  await page.getByLabel('Confirm Password').fill('1234567');
   await page.getByRole('button', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('name@.com').click();
+  await page.getByPlaceholder('name@.com').fill('userr123@gmail.com');
+  await page.getByPlaceholder('••••••••').click();
+  await page.getByPlaceholder('••••••••').fill('1234567');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page).toHaveURL('http://localhost:3001/');
+
+  await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+
 });
