8d2704a444e5ed9423f73a92a5c3cdb1e2b9e2c6
Zahra Khodabakhshian <zahrakhodabakhshian54@gmail.com>
2024-07-21 12:04:29 +0200

    fix navbar on mobile

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import hat from "../assets/images/hat.png";
+
 
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   const handleLogout = () => {
     localStorage.removeItem('token');
     navigate('/login');
@@ -24,10 +30,11 @@ const Navbar = () => {
     { path: '/features', text: 'Features' },
   ];
 
+
   return (
     <header>
       <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-darkgray">
-        <div className="flex flex-wrap justify-between items-center mx-auto">
+        <div className="flex flex-wrap justify-between items-center mx-auto  relative lg:static">
           <Link to="/" className="flex items-center gap-1">
             <img src={hat} alt="logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Mafia</span>
@@ -42,21 +49,33 @@ const Navbar = () => {
                 Log in
               </Link>
             )}
-            <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-400 rounded-lg lg:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-              <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </button>
-          </div>
-          <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              {items.map((item) => (
-                <li key={item.path}>
-                  <Link to={item.path} className={getLinkClass(item.path)}>{item.text}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+            
+            <button 
+            onClick={toggleMenu}
+            type="button" 
+            className="inline-flex items-center p-2 ml-1 text-sm text-gray-400 rounded-lg lg:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
+            aria-controls="mobile-menu-2" 
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className={`w-6 h-6 ${isMenuOpen ? 'hidden' : 'block'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+            </svg>
+            <svg className={`w-6 h-6 ${isMenuOpen ? 'block' : 'hidden'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+            </svg>
+          </button>
+        </div>
+        <div className={`justify-between items-center w-full lg:flex lg:w-auto absolute top-full lg:static bg-slate lg:bg-transparent lg:order-1 ${isMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu-2">
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            {items.map((item) => (
+              <li key={item.path}>
+                <Link to={item.path} className={getLinkClass(item.path)}>{item.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        
         </div>
       </nav>
     </header>
